1e04ec1300fa14d44144af6872bdf79716eb917a
Patrick Smyth <patrick@iotaschool.com>
2023-09-07 23:34:34 -0400

    Added wsgi script

--- wsgi.py
+++ wsgi.py
@@ -1,23 +1,4 @@
-import os
-import subprocess
-
-from flask import Flask, abort
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return 'Webhooks'
-
-@app.route('/<script>')
-def run(script):
-
-    files = os.listdir()
-
-    if script not in files:
-        abort(404)
-
-
-    rc = subprocess.call(f"./{script}")
-
-    return f'{rc}'
-
+from run import app
+ 
+if __name__ == '__main__':
+   app.run()
